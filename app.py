fd11419320aa122ae183aa72c0c53351c7f669e6
Benyamin Naranjani <bnaranjani@gmail.com>
2025-02-18 12:51:18 +0100

    Fixes and updates

--- app.py
+++ app.py
@@ -99,8 +99,9 @@ def predict():
             row[f"{model_name}_prob"] = round(y_prob[i], 4)
         
         results_table.append(row)
-    
+ 
     return render_template('index.html', results=results_table)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))  # Use PORT from environment
+    app.run(debug=True, host='0.0.0.0', port=port)
